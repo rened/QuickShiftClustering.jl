602a4817b04a2180e589cdf20f47a972824e63a9
Viktor Petukhov <viktor.s.petuhov@ya.ru>
2018-05-17 18:48:08 +0300

    Remove FunctionalDataUtils dependency. Compatibility with julia 0.6. Minimum v0.5

--- src/memtest.jl
+++ src/memtest.jl
@@ -1,5 +1,4 @@
-using FunctionalData, QuickShiftClustering
-data = @p map unstack(1:10) (x->10*randn(2,1).+randn(2,1000)) | flatten
+data = vcat([10*randn(2,1).+randn(2,1000) for i in 1:10]...)
 @time a = quickshift(data)
 Profile.clear_malloc_data()
 @time a = quickshift(data)
